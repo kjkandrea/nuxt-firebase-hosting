f957ec823b0200df6e65c27f7eb5fe382173c074
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-23 15:52:01 +0900

    create nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Nuxt + Firebase hosting
+        </v-card-title>
+        <v-card-text>
+          <p>test code</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
